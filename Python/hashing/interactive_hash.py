f371d69c4cb7a4e1c54cb91d794fe78747d3992f
zachjohnson <johnsza5@oregonstate.edu>
2023-10-29 16:50:54 -0700

    hoorah

--- Python/hashing/interactive_hash.py
+++ Python/hashing/interactive_hash.py
@@ -6,7 +6,6 @@ hash_table = {}
 
 def add_profile_to_table(hash_table, profile):
     """Adds a profile to the hash table with a unique random key."""
-    # Generate a random key and check for its uniqueness
     while True:
         profile_key = "Profile_" + str(random.randint(10000000, 99999999))
         if profile_key not in hash_table:
@@ -15,22 +14,19 @@ def add_profile_to_table(hash_table, profile):
     hash_table[profile_key] = profile
     return profile_key  # Return the generated key so it can be displayed to the user
 
-
-def save_to_csv(hash_table, filename="profiles.csv"):
+def save_to_csv(hash_table, filename="default.csv"):
     """Saves the hash table to a CSV file."""
+    if not filename.endswith('.csv'):
+        filename += '.csv'
+    
     with open(filename, 'w', newline='') as file:
         writer = csv.writer(file)
-        # Write the header
         writer.writerow(["Profile Key", "Name", "Age", "City", "State", "Country"])
         
-        # Write data
         for key, profile in hash_table.items():
             writer.writerow([key, profile.get('name', ''), profile.get('age', ''), profile.get('city', ''), profile.get('state', ''), profile.get('country', '')])
 
 def input_profile():
-    """Interactively inputs a profile."""
-    
-    # Name validation
     while True:
         name = input("Enter the name: ").strip()
         if not name or not all(word.isalpha() for word in name.split()):
@@ -38,7 +34,6 @@ def input_profile():
             continue
         break
     
-    # Age validation
     while True:
         age = input("Enter the age: ").strip()
         if not age.isdigit() or not (0 <= int(age) <= 120):
@@ -46,7 +41,6 @@ def input_profile():
             continue
         break
     
-    # City validation
     while True:
         city = input("Enter the city: ").strip()
         if not city or not all(word.isalpha() for word in city.split()):
@@ -54,14 +48,11 @@ def input_profile():
             continue
         break
 
-    # Optional State validation
     state = input("Enter the state (leave empty if not applicable): ").strip()
     while state and not all(word.isalpha() for word in state.split()):
         print("Please enter a valid state name (alphabets only) or leave it empty.")
         state = input("Enter the state (leave empty if not applicable): ").strip()
 
-
-    # Country validation
     while True:
         country = input("Enter the country: ").strip()
         if not country or not all(word.isalpha() for word in country.split()):
@@ -96,13 +87,13 @@ def main():
             print(f"Profile saved with key: {generated_key}")
             
         elif choice == '2':
-            change_filename = input("Would you like to change the default filename (profiles.csv)? (yes/no) ").strip().lower()
+            change_filename = input("Would you like to change the default filename (default.csv)? (yes/no) ").strip().lower()
             if change_filename == 'yes':
-                filename = input("Enter the new filename: ")
+                filename = input("Enter the new filename (no need to add .csv, it will be added automatically): ")
                 save_to_csv(hash_table, filename)
             else:
                 save_to_csv(hash_table)
-            print(f"Saved to {filename if change_filename == 'yes' else 'profiles.csv'}")
+            print(f"Saved to {filename if change_filename == 'yes' else 'default.csv'}")
             
         elif choice == '3':
             print("Goodbye!")
--- Python/hashing/merge.py
+++ Python/hashing/merge.py
@@ -1,26 +1,41 @@
 import csv
+import os
 
-def merge_csvs_with_prefix(csv1, csv2, output_filename):
-    """Merge two CSV files into one with a unique prefix added to keys from each file."""
-    
-    with open(csv1, 'r') as file1, open(csv2, 'r') as file2, open(output_filename, 'w', newline='') as output_file:
-        reader1 = csv.reader(file1)
-        reader2 = csv.reader(file2)
+def merge_csvs_with_prefix(filenames, output_filename):
+    """Merge multiple CSV files into one, with a prefix derived from each filename added to keys from each file."""
+
+    with open(output_filename, 'w', newline='') as output_file:
         writer = csv.writer(output_file)
+        
+        for filename in filenames:
+            # Extract prefix from filename (without extension)
+            prefix = os.path.splitext(os.path.basename(filename))[0]
+            
+            with open(filename, 'r') as file:
+                reader = csv.reader(file)
+                
+                # Write headers from the first CSV, skip headers for the rest
+                if filename == filenames[0]:
+                    writer.writerow(next(reader))
+                else:
+                    next(reader)
+                
+                # Write content of current CSV with its filename as a prefix
+                for row in reader:
+                    row[0] = f"{prefix}_{row[0]}"
+                    writer.writerow(row)
 
-        # Write headers (assuming both CSVs have the same headers)
-        writer.writerow(next(reader1))
-        next(reader2)  # Skip header of the second CSV
+if __name__ == "__main__":
+    # Prompt the user for number of files to merge
+    n = int(input("Enter the number of CSV files you want to merge: "))
+    
+    # Collect filenames
+    filenames = []
+    for i in range(n):
+        file_name = input(f"Enter the name of CSV file {i + 1}: ")
+        filenames.append(file_name)
+    
+    output_filename = input("Enter the name for the merged output file: ")
 
-        # Write content of first CSV with a prefix 'A_'
-        for row in reader1:
-            row[0] = "A_" + row[0]
-            writer.writerow(row)
-
-        # Write content of the second CSV with a prefix 'B_'
-        for row in reader2:
-            row[0] = "B_" + row[0]
-            writer.writerow(row)
-
-# Usage example:
-merge_csvs_with_prefix('profiles.csv', 'flies.csv', 'merged_file.csv')
+    merge_csvs_with_prefix(filenames, output_filename)
+    print(f"Files {' ,'.join(filenames)} have been merged into {output_filename}.")
